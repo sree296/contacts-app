2711b2c3cfce3a07f185806ab1477f18ead12395
sree296 <nivas296@gmail.com>
2021-06-17 16:20:57 +0530

    Search functionality

---
+++ src/components/Nav.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Nav = () => {
+    return(
+            <nav className="text-light text-center navbar navbar-dark bg-dark">
+               <h2 className="container">Product Search</h2>
+            </nav>
+    );
+}
+export default Nav;
